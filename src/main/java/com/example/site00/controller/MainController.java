f0bf44246d52afe582288a7b015be5433a246de8
AuArgen <argenazizbekuulu3@gmail.com>
2021-08-10 21:04:49 +0600

    Some

--- src/main/java/com/example/site00/controller/MainController.java
+++ src/main/java/com/example/site00/controller/MainController.java
@@ -1,14 +1,15 @@
 package com.example.site00.controller;
+
 import com.example.site00.domain.Message;
+import com.example.site00.domain.User;
 import com.example.site00.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -18,22 +19,26 @@ public class MainController {
     @GetMapping("/")
     public String greeting(
                            Map<String,Object> model) {
-        return "greeting";
+        return "/greeting";
     }
     @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages",messages);
-        return "main";
+        return "/main";
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String,Object>model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String,Object>model) {
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages",messages);
-        return "main";
+        return "/main";
     }
 
     @PostMapping("/filter")
